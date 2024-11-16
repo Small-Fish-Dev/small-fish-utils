eb913912fde8900493cb2556c6721ab217fa2ab1
Matthew Zegar <matthewzegar@gmail.com>
2024-11-16 10:16:43 -0800

    Hash code extension, singleton

---
+++ Libraries/SmallFishUtils/Code/Assembly.cs
@@ -0,0 +1,8 @@
+global using Sandbox;
+global using Editor;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System;
+global using System.IO;
+global using System.Text;
+global using System.Text.Json;
---
+++ Libraries/SmallFishUtils/Code/SingletonComponent.cs
@@ -0,0 +1,42 @@
+namespace SmallFishUtils;
+
+// Thanks Facepunch
+public abstract class SingletonComponent<T> : Component, IHotloadManaged where T : SingletonComponent<T>
+{
+	public static T Instance { get; private set; }
+
+	protected override void OnAwake()
+	{
+		if ( Instance.IsValid() )
+		{
+			Destroy();
+			return;
+		}
+
+		if ( Active )
+		{
+			Instance = (T)this;
+		}
+	}
+
+	void IHotloadManaged.Destroyed( Dictionary<string, object> state )
+	{
+		state["IsActive"] = Instance == this;
+	}
+
+	void IHotloadManaged.Created( IReadOnlyDictionary<string, object> state )
+	{
+		if ( state.GetValueOrDefault( "IsActive" ) is true )
+		{
+			Instance = (T)this;
+		}
+	}
+
+	protected override void OnDestroy()
+	{
+		if ( Instance == this )
+		{
+			Instance = null;
+		}
+	}
+}
