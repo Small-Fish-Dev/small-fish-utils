85632b2a81e6114c0dc0cbc8445f00d31eb7b868
Matthew Zegar <32153021+matekdev@users.noreply.github.com>
2026-01-13 17:55:07 -0800

    Rename instance to current

--- Libraries/SmallFishUtils/Code/Components/Singleton.cs
+++ Libraries/SmallFishUtils/Code/Components/Singleton.cs
@@ -3,11 +3,11 @@ namespace SmallFishUtils;
 // Originally taken from Facepunch source code, thank you!
 public abstract class Singleton<T> : Component, IHotloadManaged where T : Singleton<T>
 {
-	public static T Instance { get; private set; }
+	public static T Current { get; private set; }
 
 	protected override void OnAwake()
 	{
-		if ( Instance.IsValid() )
+		if ( Current.IsValid() )
 		{
 			Log.Warning( $"Multiple Singletons found of type: {TypeLibrary.GetType<T>().Name}" );
 			Destroy();
@@ -16,20 +16,20 @@ public abstract class Singleton<T> : Component, IHotloadManaged where T : Single
 
 		if ( Active )
 		{
-			Instance = (T)this;
+			Current = (T)this;
 		}
 	}
 
 	void IHotloadManaged.Destroyed( Dictionary<string, object> state )
 	{
-		state["IsActive"] = Instance == this;
+		state["IsActive"] = Current == this;
 	}
 
 	void IHotloadManaged.Created( IReadOnlyDictionary<string, object> state )
 	{
 		if ( state.GetValueOrDefault( "IsActive" ) is true )
 		{
-			Instance = (T)this;
+			Current = (T)this;
 		}
 	}
 
@@ -37,7 +37,7 @@ public abstract class Singleton<T> : Component, IHotloadManaged where T : Single
 	{
 		if ( Instance == this )
 		{
-			Instance = null;
+			Current = null;
 		}
 	}
 }
